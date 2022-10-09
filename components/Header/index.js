d86b313cd9a471577f6e75136683532b2b5f4577
Nafsirudin <nafsirudin8gmail.com>
2022-10-09 08:55:32 +0700

    rm packagelock

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,6 +8,7 @@ const Header = () => {
         <li><Link href="/">Home</Link></li>
         <li><Link href="/blog">Blog</Link></li>
         <li><Link href="/users">Users</Link></li>
+        <li><Link href="/photos">Photos</Link></li>
       </ul>
     </header>
   )
