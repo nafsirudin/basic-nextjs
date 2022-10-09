309b6c9e2ae85300133584d9e33aecb5231c0fe0
Nafsirudin <nafsirudin8gmail.com>
2022-10-09 09:07:38 +0700

    solve error eslint

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,16 +1,17 @@
-import { useRouter } from "next/router";
-import Layout from "../../components/Layout";
+import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
 const Users = (props) => {
   const router = useRouter();
+  const { dataUsers } = props;
   return (
     <Layout pageTitle="Users">
       <h1>Users Page</h1>
       <ul>
         {
-          props.dataUsers?.map((user, index) => {
-            return(
-              <li key={index} onClick={()=> { router.push(`/users/${user.id}`)}}>{user.name}</li>
+          dataUsers?.map((user) => {
+            return (
+              <li key={user.id} onClick={() => { router.push('users') }}>{user.name}</li>
             );
           })
         }
@@ -21,14 +22,13 @@ const Users = (props) => {
 
 export default Users;
 
-
-//get static props (tidak cocok untuk website yg dinamis) / static generate
-export async function getStaticProps(){
+// get static props (tidak cocok untuk website yg dinamis) / static generate
+export async function getStaticProps() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const dataUsers = await response.json();
   return {
     props: {
-      dataUsers: dataUsers
-    }
+      dataUsers,
+    },
   }
 }
